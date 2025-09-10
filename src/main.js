807d2b0a3700c1f35a1c6ede98d708855b4c650c
xiyuesaves <xiyuesaves@gmail.com>
2025-09-10 13:58:39 +0800

    chore: 移除不必要的前置插件检测

--- src/main.js
+++ src/main.js
@@ -1,12 +1,8 @@
 import { replaceMiniAppArk, log } from "./replaceMiniAppArk.js";
 
 try {
-  if (global.IpcInterceptor) {
-    IpcInterceptor.onIpcSend(replaceMiniAppArk);
-    log("已加载");
-  } else {
-    throw new Error("未找到 IpcInterceptor，请安装前置插件 QWQNT-IpcInterceptor");
-  }
+  IpcInterceptor.onIpcSend(replaceMiniAppArk);
+  log("已加载");
 } catch (err) {
   log("出现错误：", err);
 }
